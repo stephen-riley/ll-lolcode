725b6b9a5bfc317c3d3de9a1d050988050d901ef
Stephen Riley <jstephen.riley@yahoo.com>
2020-10-10 20:58:12 -0700

    add sln file and test project

---
+++ LolCode.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LolCode.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
